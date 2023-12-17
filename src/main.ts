cd8404713858ed12e0ac575c1aaa7425da692b52
myuon <ioi.joi.koi.loi@gmail.com>
2023-12-17 14:36:00 +0900

    feat: use webgl2

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 const main = () => {
   const canvas = document.querySelector("#glcanvas")! as HTMLCanvasElement;
-  const gl = canvas.getContext("webgl")!;
+  const gl = canvas.getContext("webgl2")!;
 
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
