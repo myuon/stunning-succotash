fe431c824af2a63b37a10506cc0b32c886294b2d
myuon <ioi.joi.koi.loi@gmail.com>
2023-12-17 17:25:02 +0900

    feat: hello, triangle for webgl

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,111 @@
+import shaderVertSource from "./glsl/shader.vert?raw";
+import shaderFragSource from "./glsl/shader.frag?raw";
+import { mat4 } from "gl-matrix";
+
+const compileShader = (
+  gl: WebGL2RenderingContext,
+  shader: WebGLShader,
+  source: string
+) => {
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    console.error(gl.getShaderInfoLog(shader));
+  }
+};
+
+const createProgram = (
+  gl: WebGL2RenderingContext,
+  vertShader: WebGLShader,
+  fragShader: WebGLShader
+) => {
+  const program = gl.createProgram();
+  if (!program) {
+    console.error("Failed to create program");
+    return;
+  }
+
+  gl.attachShader(program, vertShader);
+  gl.attachShader(program, fragShader);
+  gl.linkProgram(program);
+
+  if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    gl.useProgram(program);
+
+    return program;
+  } else {
+    console.error(gl.getProgramInfoLog(program));
+  }
+};
+
+const createVbo = (gl: WebGL2RenderingContext, data: number[]) => {
+  const vbo = gl.createBuffer();
+  if (!vbo) {
+    console.error("Failed to create buffer");
+    return;
+  }
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  return vbo;
+};
+
 const main = () => {
   const canvas = document.querySelector("#glcanvas")! as HTMLCanvasElement;
-  const gl = canvas.getContext("webgl2")!;
+  const gl = canvas.getContext("webgl2");
+  if (!gl) {
+    console.error("Failed to get WebGL context");
+    return;
+  }
 
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.clearDepth(1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  const vshader = gl.createShader(gl.VERTEX_SHADER);
+  if (!vshader) {
+    console.error("Failed to create vshader");
+    return;
+  }
+  const fshader = gl.createShader(gl.FRAGMENT_SHADER);
+  if (!fshader) {
+    console.error("Failed to create fshader");
+    return;
+  }
+
+  compileShader(gl, vshader, shaderVertSource);
+  compileShader(gl, fshader, shaderFragSource);
+  if (!vshader || !fshader) return;
+
+  const program = createProgram(gl, vshader, fshader);
+  if (!program) return;
+
+  const location = gl.getAttribLocation(program, "position");
+  const attribStride = 3;
+  const vertexPosition = [0.0, 1.0, 0.0, 1.0, 0.0, 0.0, -1.0, 0.0, 0.0];
+
+  const vbo = createVbo(gl, vertexPosition);
+  if (!vbo) return;
+  gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+  gl.enableVertexAttribArray(location);
+  gl.vertexAttribPointer(location, attribStride, gl.FLOAT, false, 0, 0);
+
+  const mMatrix = mat4.identity(mat4.create());
+  const vMatrix = mat4.identity(mat4.create());
+  const pMatrix = mat4.identity(mat4.create());
+  const mvpMatrix = mat4.identity(mat4.create());
+
+  mat4.lookAt(vMatrix, [0.0, 1.0, 3.0], [0, 0, 0], [0, 1, 0]);
+  mat4.perspective(pMatrix, 45, canvas.width / canvas.height, 0.1, 100);
+  mat4.multiply(mvpMatrix, pMatrix, vMatrix);
+  mat4.multiply(mvpMatrix, mvpMatrix, mMatrix);
+
+  const uniLocation = gl.getUniformLocation(program, "mvpMatrix");
+  gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  gl.flush();
 };
 
 main();
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+
+declare module "*?raw" {
+  const contents: string;
+  export default contents;
+}
